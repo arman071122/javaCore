fdf67060e26a65a2ad9b80b46c6521c883c14ba1
arman071122 <98600855+arman071122@users.noreply.github.com>
2024-09-08 02:02:08 +0530

    commit

--- Java/src/week4/reveres.java
+++ Java/src/week4/reveres.java
@@ -2,7 +2,7 @@ package week4;
 
 public class reveres {
     public static void main(String[] args) {
-	String s = "arman";
+	String s = "namra";
 
 	char c[] = s.toCharArray();
 	for (int i = s.length() - 1; i >= 0; i--) {
