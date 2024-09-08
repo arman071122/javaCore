b31810783848e6448eaf5f3af9c9ced60795dce3
arman071122 <98600855+arman071122@users.noreply.github.com>
2024-09-08 20:20:01 +0530

    Merge branch 'main' of https://github.com/arman071122/javaCore.git into main

--- Java/src/week4/RightTri.java
+++ Java/src/week4/RightTri.java
@@ -4,7 +4,7 @@ public class RightTri {
     public static void main(String[] args) {
 	for (int i = 1; i <= 5; i++) {
 	    for (int j = 1; j <= i; j++) {
-		System.out.print("*");
+		System.out.print("* ");
 	    }
 	    System.out.println("");
 	}
