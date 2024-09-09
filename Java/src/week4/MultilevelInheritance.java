6d9dfaa690cd6430f47a765946528fd3ed6a01c5
arman071122 <98600855+arman071122@users.noreply.github.com>
2024-09-09 11:48:34 +0530

    new commit

--- Java/src/week4/MultilevelInheritance.java
+++ Java/src/week4/MultilevelInheritance.java
@@ -1,6 +1,5 @@
 package week4;
 
-//Parent class (Superclass)
 class Animal1 {
     String name;
 
@@ -14,24 +13,20 @@ class Animal1 {
     }
 }
 
-//Child class (Subclass) that inherits from Animal
 class Dog1 extends Animal1 {
     void bark() {
 	System.out.println(name + " is barking.");
     }
 }
 
-//Class that inherits from Dog
 public class MultilevelInheritance extends Dog1 {
     public static void main(String[] args) {
 	MultilevelInheritance dog1 = new MultilevelInheritance();
 	dog1.name = "Shreyas";
 
-	// Calling methods from the Animal superclass
 	dog1.eat();
 	dog1.sleep();
 
-	// Calling method from the Dog class
 	dog1.bark();
     }
 }
