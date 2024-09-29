faf6f10d50ffc11513e235b1f44b4ffa8d7f4147
arman071122 <98600855+arman071122@users.noreply.github.com>
2024-09-29 23:42:08 +0530

    Merge branch 'main' of https://github.com/arman071122/javaCore.git into main

--- Java/src/week6/OruFun.java
+++ Java/src/week6/OruFun.java
@@ -2,7 +2,7 @@ package week6;
 
 import java.util.Scanner;
 
-public class ConditionalCheck {
+public class OruFun {
     public static void main(String[] args) {
 	Scanner scanner = new Scanner(System.in);
 
