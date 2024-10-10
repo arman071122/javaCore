b1359954c625d9553fad534d8076dfa0d7d5ffff
arman071122 <98600855+arman071122@users.noreply.github.com>
2024-10-10 22:32:30 +0530

    Merge branch 'main' of https://github.com/arman071122/javaCore.git into main

---
+++ Java/src/week8/ListIteratiorEg.java
@@ -0,0 +1,52 @@
+package week8;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+public class ListIteratiorEg {
+    public static void main(String[] args) {
+	ArrayList<String> fruit = new ArrayList<String>();
+
+	fruit.add("apple");
+	fruit.add("banana");
+	fruit.add("mango");
+
+	ListIterator<String> li = fruit.listIterator();
+	while (li.hasNext())
+	    System.out.println(li.next());
+	li.add("dates");
+
+	System.out.println();
+
+	while (li.hasPrevious())
+	    System.out.println(li.previous());
+
+	System.out.println();
+
+	li.add("jackfruit");
+	li = fruit.listIterator();
+	while (li.hasNext())
+	    System.out.println(li.next());
+
+	System.out.println();
+
+	li.previous();
+	li.previous();// points to mango
+	li.set("green mango");// replaces mango with green mango
+
+	li = fruit.listIterator();
+	while (li.hasNext())
+	    System.out.println(li.next());
+
+	Iterator i = fruit.iterator();
+	while (i.hasNext())
+	    System.out.println(i.next());
+
+	li = fruit.listIterator();
+	while (li.hasNext()) {
+	    li.next();
+	    li.remove();
+	}
+    }
+}
